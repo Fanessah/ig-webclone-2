469f899e9ea3d58c3d9b7c8e1cafe0c94605ee9d
Fanessa <fanessah@gmail.com>
2022-11-16 15:05:30 -0500

    updated feed component

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,9 +1,22 @@
-export default function Feed(){
-    return(
-        <>
-        <header>
-            Feed Goes Here 
-        </header>
-        </>
+import { useState, useEffect } from "react"
+
+export default function Feed() {
+    const [photoList, setPhotoList] = useState()
+    useEffect(() => {
+        fetch("https://express-ts-fh.web.app/photos")
+            .then(results => results.json())
+            .then(data => setPhotoList(data))
+            .cath(alert)
+    }, [setPhotoList])
+
+
+    return (
+        <section>
+            {!photoList
+                ? <p>Loading...</p>
+                : <p> {photoList.length}</p>
+            }
+        </section>
+
     )
 }
