93b731ea5f4e2c41a764896eb7fcf5f65179b834
Fanessa <fanessah@gmail.com>
2022-11-16 15:39:13 -0500

    finished with form in Upload

--- src/components/Upload.jsx
+++ src/components/Upload.jsx
@@ -11,7 +11,24 @@ export default function Upload({setShowUpload}) {
         <>
         <Modal title="Upload Photo" open={true} footer={null} onCancel={closeModal} >
             <Form label col ={{ span:8}} wrapperCol={{span:16}} onFinish={handleNewPhoto}>
-                <Form.Item label=""
+                <Form.Item label="User Name" name="username">
+                    <Input required/>
+                </Form.Item>
+                <Form.Item label="Profile Picture URL" name="ProfilePic">
+                    <Input required/>
+                </Form.Item>
+                <Form.Item label="Photo" name="Photo">
+                    <Input required/>
+                </Form.Item>
+                <Form.Item label="Description" name="description">
+                    <Input required/>
+                </Form.Item>
+                <Form.Item wrapperCol={{offset:8,span:16}}>
+                    <Button type="primary" htmlType="submit"> Save Photo</Button>
+                    <Input required/>
+                </Form.Item>
+                
+
             </Form>
         </Modal>
         </>
